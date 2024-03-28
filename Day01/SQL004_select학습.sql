15a62d3b1bd64616903993346feddcb4f3c8e725
Roy-Lee <ljunghwan928@naver.com>
2024-03-28 17:22:52 +0900

    update

--- Day01/SQL004_select학습.sql
+++ Day01/SQL004_select학습.sql
@@ -14,3 +14,55 @@ SELECT *
 
 SELECT bookid, bookname, publisher, price
 	FROM Book;
+
+-- 도서에서 출판사를 검색하시오 (중복제거)
+SELECT DISTINCT publisher
+	FROM Book;
+
+-- 조건검색(조건 연산자 사용)
+-- WHERE 절 = 가격이 2만원 미만인 도서를 검색하시오
+
+SELECT * 
+	FROM Book
+	where price < 20000;
+
+-- 가격이 1만원 이상, 2만원 이하인 도서를 검색
+SELECT *
+	FROM Book
+	WHERE price BETWEEN 10000 AND 20000;
+
+-- 출판사가 굿스포츠와 대한미디어 도서를 검색
+SELECT *
+	FROM Book
+	WHERE publisher IN ('굿스포츠', '대한미디어');
+
+-- 출판사가 굿스포츠와 대한미디어 아닌 도서를 검색
+SELECT *
+	FROM Book
+	WHERE publisher NOT IN ('굿스포츠', '대한미디어');
+
+-- 축구의 역사를 출한한 출판사 검색
+SELECT bookname, publisher
+	FROM Book
+	WHERE bookname = '축구의 역사';
+
+-- 도서 이름에 축구가 포함된 출판사를 검색
+SELECT bookname, publisher
+	FROM Book
+	WHERE bookname LIKE '축구%'; -- 축구라는 글자로 시작
+
+
+SELECT bookname, publisher
+	FROM Book
+	WHERE bookname LIKE '%축구'; -- 축구라는 글자로 끝
+
+
+SELECT bookname, publisher
+	FROM Book
+	WHERE bookname LIKE '%축구%'; -- 축구라는 글자로 시작과 끝
+
+
+-- 도서 이름에 축구,야구가 포함된 출판사를 검색
+SELECT bookname, publisher
+	FROM Book
+	WHERE bookname LIKE '_구%'; -- _(무슨글자든 한글자가 들어간) 구 라는 글자로 시작
